988e7064c6d0dc581341b3e3197cb1f9f94fafcd
monil-georgian <65424895+monil-georgian@users.noreply.github.com>
2020-06-21 23:51:29 +0530

    Add files via upload

---
+++ midterm.py
@@ -0,0 +1,358 @@
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))
+print("5 is {}".format(type(5)))
+
+print("5.0 is {}".format(type(5.0)))
+
+print("5 > 1 is {}".format(type(5 > 1)))
+
+print("'5' is {}".format(type('5')))
+
+print("5 * 2 is {}".format(type(5 * 2)))
+
+print("'5' * 2 is {}".format(type('5' * 2)))
+
+print("'5' + '2' is {}".format(type('5' + '2')))
+
+print("5 / 2 is {}".format(type(5 / 2)))
+
+print("5 // 2 is {}".format(type(5 // 2)))
+
+print("[5, 2, 1] is {}".format(type([5, 2, 1])))
+
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))            
+            
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))    
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))    
+print("5 in [1, 4, 6] is {} ".format(type(5 in [1, 4, 6])))
+
+import math
+print("math.pi is {}".format(type(math.pi)))    
